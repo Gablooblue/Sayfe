a137fa0d0e3544d7d4f6c4995a7869d911db9be1
gablooblue <gabrielhenry.lopez@gmail.com>
2017-07-09 23:15:57 +0800

    Changed search routing

--- Sayfe/app/controllers/search_controller.rb
+++ Sayfe/app/controllers/search_controller.rb
@@ -1,9 +1,8 @@
-class ResultsController < ApplicationController
+class SearchController < ApplicationController
 
     def index
 	@users = User.all
 	@q = @users.ransack(params[:q])
 	@results = @q.result(distinct: true)
     end
-
 end
--- Sayfe/config/routes.rb
+++ Sayfe/config/routes.rb
@@ -49,6 +49,8 @@ Rails.application.routes.draw do
     resources :splash, only: [:index]
 
     resources :dashboard, only: [:index]
+
+    resources :search, only: [:index]
  
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html 
     root 'splash#index' 
